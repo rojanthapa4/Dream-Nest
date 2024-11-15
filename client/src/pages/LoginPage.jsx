2e389d65f9880279565e151e4ca3290dc7463501
Rojan <rojant061@gmail.com>
2024-11-15 19:27:24 +0545

    Login Setup complited

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,50 @@
 import React, { useState } from "react";
 import '../styles/Login.scss'
+import { setLogin } from "../redux/state";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate(); 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      /*Get data after fetching*/
+      const loggedIn = await response.json();
+
+      if (loggedIn) {
+        dispatch(
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate('/')
+      }
+
+    } catch (error) {
+      console.log('login Failed', error.message)      
+    }
+  }
+
   return (
     <div className="login">
       <div className="login_content">
-        <form className="login_form">
+        <form className="login_form" onSubmit={handleSubmit}>
           <input 
           type="email" 
           placeholder="Email" 
